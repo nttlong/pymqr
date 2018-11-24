d4ac0e2b2f695e7250f3a3adcabc5368a9dd0db4
nttlong <zugeliang2000@gamil.com>
2018-11-24 19:27:33 +0700

    save

--- pymqr/__init__.py
+++ pymqr/__init__.py
@@ -54,8 +54,8 @@ def compile(exr):
 import pyfuncs as funcs
 docs=__docs__()
 filters = __filters__()
-from pymodel import Index,IndexOption,FieldInfo
-def create_index(fields,options):
-    import pymodel
-    return pymodel.Index(fields,options)
-from pydocs import BSON_select, BSON_doc
+# from pymodel import Index,IndexOption,FieldInfo
+# def create_index(fields,options):
+#     import pymodel
+#     return pymodel.Index(fields,options)
+# from pydocs import BSON_select, BSON_doc
